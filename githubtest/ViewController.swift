d11a5c07213d49e8b2128a702d64a5f6e5f8b239
Saintqqq <Saint590457@gmail.com>
2020-10-15 21:23:32 +0800

    test 456

--- githubtest/ViewController.swift
+++ githubtest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("123 test")
+        print("456")
     }
 
 
